030b70ab8029b78aa6d05aab32be813fd45f1006
mucrow <120820315+mucrow@users.noreply.github.com>
2024-05-04 13:49:21 -0400

    Create git repository

---
+++ player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+
+@export var jump_velocity = -400.0
+@export var move_speed = 300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+  # Add the gravity.
+  if not is_on_floor():
+    velocity.y += gravity * delta
+
+  # Handle jump.
+  if Input.is_action_just_pressed("jump") and is_on_floor():
+    velocity.y = jump_velocity
+
+  # Get the input direction and handle the movement/deceleration.
+  var direction = Input.get_axis("move_left", "move_right")
+  if direction:
+    velocity.x = direction * move_speed
+  else:
+    velocity.x = move_toward(velocity.x, 0, move_speed)
+
+  move_and_slide()
