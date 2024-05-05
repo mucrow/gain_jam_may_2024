3e3d60f436f31159503b94951240339d69ae73b4
mucrow <120820315+mucrow@users.noreply.github.com>
2024-05-05 14:58:24 -0400

    Add bug

---
+++ enemy.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+# bugs don't jump and gods don't bleed
+@export var jump_velocity = -400.0
+@export var move_speed = 100.0
+@export var gravity = 980.0
+
+var direction = -1.0
+
+
+func _physics_process(delta):
+  if not is_on_floor():
+    velocity.y += gravity * delta
+
+  velocity.x = direction * move_speed
+  move_and_slide()
+
+
+func on_turnaround_timeout():
+  direction *= -1.0
--- player.gd
+++ player.gd
@@ -3,9 +3,7 @@ extends CharacterBody2D
 
 @export var jump_velocity = -400.0
 @export var move_speed = 300.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var gravity = 980.0
 
 
 func _physics_process(delta):
