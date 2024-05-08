031723e15c4c358d0a1436dfa2393fd97a763b67
ahodori <supersamfast@gmail.com>
2024-05-07 22:39:47 -0400

    store x speed when hitting wall, walljump will use that speed (or minimum)

--- mobs/player.gd
+++ mobs/player.gd
@@ -29,6 +29,8 @@ var in_dash_cooldown: bool = false
 var walltouch_velocity: Vector2
 var justtouchedwall: bool = false
 
+var previous_velocity: Vector2
+
 
 func _physics_process(dt):
   if state == State.Free:
@@ -37,7 +39,8 @@ func _physics_process(dt):
     state_launched_physics_process(dt)
   else:
     push_warning('unhandled state %s' % state)
-    state = State.Free
+    state = State.Free 
+  previous_velocity = velocity
 
 
 func state_free_physics_process(dt):
@@ -112,13 +115,15 @@ func apply_gravity(dt):
 
 
 func check_walltouch():
-  if not justtouchedwall and is_on_wall_only():
+  var canwal = can_walljump()
+  if not justtouchedwall and canwal:
     justtouchedwall = true
-    walltouch_velocity = get_last_slide_collision().get_travel() * 400 + get_last_slide_collision().get_remainder() * 400
-    # print("storing wallhit velocity")
-    # print(walltouch_velocity)
+    #TODO: remove walltouch velocity if you leave the wall (or 1 sec until it disappears?)
+    walltouch_velocity = previous_velocity
+    print("storing wallhit velocity")
+    print(walltouch_velocity)
 
-  if not is_on_wall_only():
+  if not can_walljump():
     justtouchedwall = false
 
 
@@ -175,6 +180,8 @@ func can_walljump():
 func apply_walljump(dir):
     var min_launch_velocity = (dir * Vector2.LEFT + Vector2.UP) * walljump_strength
     var launch_velocity = min_launch_velocity
+    if abs(walltouch_velocity.x) > abs(launch_velocity.x):
+      launch_velocity.x = -walltouch_velocity.x 
     velocity.y = 0
 
     apply_launch(launch_velocity, 0.0)
