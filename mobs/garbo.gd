25d9f99f5b2a424fa2b3f51a1a999b1ecbe34fe7
mucrow <120820315+mucrow@users.noreply.github.com>
2024-05-11 20:36:06 -0400

    stac

--- mobs/garbo.gd
+++ mobs/garbo.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+class_name Garbo
 
 
 @export var move_speed = 100.0
@@ -8,25 +9,103 @@ extends CharacterBody2D
 @export var launch_strength_x: float = 1
 @export var launch_strength_y: float = 0.6
 
+@onready var collider: CollisionShape2D = $CollisionShape2D
 @onready var sprite: AnimatedSprite2D = $SpriteAnchor/AnimatedSprite2D
+@onready var sprite_anchor: Node2D = $SpriteAnchor
+@onready var stack_point: Node2D = $SpriteAnchor/StackPoint
+@onready var turnaround_timer: Timer = $TurnaroundTimer
 
 var direction = -1.0
+var sprite_height = 0
+var stacked_garbo_a_scene = preload('res://mobs/stacked_garbo_a.tscn')
+var stacked_garbo_b_scene = preload('res://mobs/stacked_garbo_b.tscn')
+var stacked_objects = []
 
 
-func _physics_process(delta):
-  if not is_on_floor():
-    velocity.y += gravity * delta
+func _ready():
+  sprite_height = sprite.sprite_frames.get_frame_texture(sprite.animation, 0).get_height()
+
+
+func _physics_process(dt):
+  if is_on_floor():
+    velocity.y = 0
+  else:
+    velocity.y += gravity * dt
 
   velocity.x = direction * move_speed
   move_and_slide()
+
+  for i in get_slide_collision_count():
+    var collision = get_slide_collision(i)
+    var other_collider = collision.get_collider()
+    if other_collider.is_in_group('garbos'):
+      on_collision_with_other_garbo(other_collider)
 
 
 func on_turnaround_timeout():
   direction *= -1.0
   sprite.flip_h = not sprite.flip_h
+  for stacked_garbo in stacked_objects:
+    stacked_garbo.toggle_sprite_flip_h()
+
+
+func on_collision_with_other_garbo(other: Garbo):
+  var angle_to_other = rad_to_deg(((other.position - position).angle()))
+  if angle_to_other >= -135.0 and angle_to_other < -45.0:
+    stack_garbo(other)
+  elif angle_to_other >= -45.0 and angle_to_other < 45.0:
+    turnaround_timer.stop()
+    turnaround_timer.start()
+    on_turnaround_timeout()
+    other.on_turnaround_timeout()
 
 
 func on_collision_with_player(player: Player):
-  var launch_velocity = (player.position - position).normalized() * launch_speed
+  var true_center = get_true_center()
+  var launch_velocity = (player.position - true_center).normalized() * launch_speed
   launch_velocity = (launch_velocity.dot(Vector2.RIGHT) * launch_strength_x * Vector2.RIGHT) + (launch_velocity.dot(Vector2.UP) * launch_strength_y * Vector2.UP)
   player.apply_launch(launch_velocity, 0.4, true)
+
+
+func stack_garbo(other: Garbo):
+  var stack_target = stacked_objects[-1] if stacked_objects.size() > 0 else self
+
+  var stacked_garbo_scene = stacked_garbo_a_scene if randi() % 2 == 0 else stacked_garbo_b_scene
+  var new_stacked_garbo: StackedGarbo = stacked_garbo_scene.instantiate()
+  stacked_objects.push_back(new_stacked_garbo)
+  stack_target.set_stacked_object(new_stacked_garbo)
+
+  update_collider()
+
+  other.queue_free()
+
+
+func set_stacked_object(object: StackedGarbo):
+  if stacked_objects.size() > 0:
+    push_warning('you called set_stacked_object on me (a root garbo), but i already had a stacked object')
+  stack_point.add_child(object)
+
+
+func get_true_center():
+  if stacked_objects.size() == 0:
+    return global_position
+  var top_stacked_object: StackedGarbo = stacked_objects[-1]
+  var top_y = top_stacked_object.stack_point.global_position.y
+  var bottom_y = global_position.y + sprite_height / 2.0
+  var true_height = bottom_y - top_y
+  var center_y = true_height / 2.0 + top_y
+  return Vector2(global_position.x, center_y)
+
+
+func get_true_height():
+  if stacked_objects.size() == 0:
+    return sprite_height
+  var top_stacked_object: StackedGarbo = stacked_objects[-1]
+  var top_y = top_stacked_object.stack_point.global_position.y
+  var bottom_y = global_position.y + sprite_height / 2.0
+  return bottom_y - top_y
+
+
+func update_collider():
+  collider.global_position = get_true_center()
+  collider.shape.height = get_true_height()
